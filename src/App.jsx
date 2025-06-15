c598ca09619f02992b8c17791f341b7d98324a68
JustinFoong <justinfjc2206@gmail.com>
2025-06-16 00:24:01 +1000

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import {Closing, Letter,Passcode,Question,Recap,Timer,Message,Music,Picture} from './components'
 import './index.css'
 function App() {
